c80bf39da0ac0056c79c9c28dcf1cd1b6a3b86ec
Chebrolu <pravallikachebrolu2003@gmail.com>
2022-11-13 12:29:53 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const addDays = require("date-fns/addDays");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const newDate = addDays(new Date(), 100);
+  response.send(
+    `${newDate.getDate()}/${newDate.getMonth() + 1}/${newDate.getFullYear()}`
+  );
+});
+
+app.listen(3000);
+
+module.exports = app;
